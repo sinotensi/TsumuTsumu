05270b0f1a0f8f129077e5b7810078c2b831b6dc
sinotensi <n1o9m8u4>
2015-02-19 04:23:20 +0900

    <Edit Login Scene> Create User Modules. Create Connecting Loading prefab. Edit LoginView.cs.

---
+++ Assets/Modules/User/User.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class User : MonoBehaviour {
+	public static string 		uUID;
+	public static string 		uName;
+	public static int 	 		uSta;
+	public static int	 		uNowExp;
+	public static int	 		uNextExp;
+	public static int	 		uMoney;
+	public static int 			uLoginTime;
+}
---
+++ Assets/Views/LoginView/LoginView.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginView : MonoBehaviour {
+	public GameObject loading;
+
+	// Use this for initialization
+	void Start () {
+		loading.SetActive( false );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( ClickCheck() )
+		{
+			ConnectLoading ();
+		}
+	}
+
+	bool ClickCheck () {
+		bool flag = false;
+		if(Input.anyKeyDown || Input.touchCount > 0)
+		{
+			flag = true;
+		}
+
+		return flag;
+	}
+
+	void ConnectLoading () {
+		loading.SetActive( true );
+	}
+}
