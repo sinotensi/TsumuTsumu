c490c085e83651cd3f860a417ffbc3e2164957ec
sinotensi <n1o9m8u4>
2015-02-19 04:16:00 +0900

    <Edit Logo Scene> input the LogoView.cs code in LogoAnimation.cs. Edit LogoView.cs to move next scene. Create SceneController.cs to controll game scene.

---
+++ Assets/Views/LogoView/LogoAnimation.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using uGUI;
+using System.Collections;
+
+public class LogoAnimation : MonoBehaviour {
+	private uTweenAlpha alphaTween;
+	
+	private int tweenCount = 0;
+	private int maxTween = 0;
+	private float coolTime = 1.5f;
+
+	public static bool End = false;
+	// Use this for initialization
+	void Start () {
+		Initialization();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	/// <summary>
+	/// Initialization this instance.
+	/// </summary>
+	void Initialization () {
+		alphaTween = this.GetComponent<uTweenAlpha>();
+	}
+	
+	/// <summary>
+	/// Tweens the end action.
+	/// </summary>
+	public void TweenEndAction () {
+		if( tweenCount <= maxTween )
+		{
+			StartCoroutine( LoopTween() );
+			tweenCount++;
+		}
+		else
+		{
+			End = true;
+		}
+	}
+	
+	/// <summary>
+	/// Loops the tween by tween count.
+	/// </summary>
+	/// <returns>Cool Time.</returns>
+	IEnumerator LoopTween () {
+		yield return new WaitForSeconds( coolTime );
+		alphaTween.from = 1;
+		alphaTween.to = 0;
+		alphaTween.enabled = true;
+		alphaTween.Play();
+	}
+}
