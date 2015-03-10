fe51340500fe8ac52a2236e124ec8bbf0c792dd8
chenyingchun <sinotensi2000@skylink-digital.com>
2015-03-10 10:06:24 +0800

    <Create GachaView scene part5> Add GachaView scene. Add GetPetView scene. Add gacha animation. Add database data. Edit screen setting. Add pet resources.

---
+++ Assets/Scripts/CameraControll.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControll : MonoBehaviour {
+	public float cameraSize;
+	public float mSpeed;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( GachaView.GaCha )
+		{
+			AnimationSet();
+		}
+	}
+
+	void AnimationSet () {
+		if( Camera.main.orthographicSize - mSpeed > cameraSize )
+		{
+			Camera.main.orthographicSize = Camera.main.orthographicSize - mSpeed;
+		}
+
+		if( transform.localPosition.y - moveSpeed > 1.95f )
+		{
+			transform.localPosition = new Vector3( 0, transform.localPosition.y - moveSpeed, -10f );
+		}
+	}
+
+	public void Initialize () {
+		Camera.main.orthographicSize = 5.0f;
+		transform.localPosition = new Vector3( 0, 3.4f, -10f );
+	}
+}
---
+++ Assets/Scripts/Exp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+public class Exp : MonoBehaviour {
+	Dictionary<string, object> PetExpStart = new Dictionary<string, object>();
+	// Use this for initialization
+	void Start () {
+		for( float i = 1; i < 200; ++i )
+		{
+			///EXP
+			//float exp = 156 * ( Mathf.Pow( ((i - 1)/98), 25.0f/10.0f ) );
+			///HP
+			//float hp = 1085 * ( Mathf.Pow( ((i - 1)/98), 1.75f/10.0f ) );
+			///ATK
+			//float atk = 644 * ( Mathf.Pow( ((i - 1)/98), 25.0f/10.0f ) );
+			///REHP
+			float heal = 159 * ( Mathf.Pow( ((i - 1)/98), 25.0f/10.0f ) );
+			PetExpStart.Add( i.ToString(), Mathf.Ceil( heal ) );
+		}
+
+		Debug.Log( Json.Serialize( PetExpStart ) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
